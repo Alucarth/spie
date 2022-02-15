0ee9a91b0ea2645a3e2fc54d1658acbb7203971b
alucarth <ltorrezs2008@gmail.com>
2022-02-15 14:05:53 -0400

    seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,9 +14,16 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersSeeder::class);
         // $this->call(ProgramacionMedioPlazoSeeder::class);
         // $this->call(YearsSeeder::class);
-        $this->call(MonthSeeder::class);
-        $this->call(ProgrammaticStructureSeeder::class);
-        $this->call(ProgrammaticOperationSeeder::class);
-        $this->call(AlertTableSeeder::class);
+        // $this->call(MonthSeeder::class);
+        // $this->call(ProgrammaticStructureSeeder::class);
+        // $this->call(ProgrammaticOperationSeeder::class);
+        // $this->call(AlertTableSeeder::class);
+
+        $this->call(SectoresSeeder::class);
+        $this->call(FormularioSeeder::class);
+        $this->call(TipoAccionSeeder::class);
+        $this->call(TipoPlanSeeder::class);
+        $this->call(TipoProgramacionSeeder::class);
+
     }
 }
